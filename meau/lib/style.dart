f55cb9c5e7f3375375b5066e65e9734c7d2eca8f
pablomuro <pablo.muro@gmail.com>
2019-09-02 23:36:46 -0300

    style

--- meau/lib/style.dart
+++ meau/lib/style.dart
@@ -4,11 +4,15 @@ import 'package:flutter/material.dart';
 //Text Sizes Constants
 const LargeTextSize = 72.0;
 const MediumTextSize = 16.0;
+const TextSize14 = 14.0;
 const SmallTextSize = 12.0;
+
+const AppTitleTextSize = 20.0;
 
 const String CourgettFontFamily = 'Courgette';
 
 //Color constant
+const Color DefaultStrongreGrennColor = Color(0xff589b9b);
 const Color DefaultGrennColor = Color(0xff88c9bf);
 const Color DefaultYellowColor = Color(0xffffd358);
 const Color DefaultTextColor = Color(0xff757575);
