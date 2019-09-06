fdb0ac563117cdac07b5f7b845a92762f25e0a95
viniciusaguiarmonteiro <viniciusaguiarmonteiro@hotmail.com>
2019-09-06 20:09:10 -0300

    teste commit branch screen/termos

---
+++ meau/lib/screens/termosScreen/termosScreen.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:meau/screens/legalScreen/texts.dart';
+import 'package:meau/style.dart';
+import 'package:meau/widgets/AppDrawer/AppDrawer.dart';
